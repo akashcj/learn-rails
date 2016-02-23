1dfc1642022361ae07014f716a2090511fa6a606
Akash <akashcj@gmail.com>
2016-02-23 10:59:53 +0530

    Created Contact Page

---
+++ app/controllers/contacts_controller.rb
@@ -0,0 +1,29 @@
+class ContactsController < ApplicationController
+
+=begin
+	def process_form
+		Rails.logger.debug "DEBUG: params are #{params}"
+		flash[:notice] = "Recieved  request from #{params[:contact][:name]} who can be reached at #{params[:contact][:email]}"
+		redirect_to root_path
+	end
+=end
+	def new
+		@contact = Contact.new
+	end
+
+	def create
+		@contact = Contact.new (secure_params)
+		if @contact.valid?
+			flash[:notice] = "Message has come from #{@contact.name}"
+=begin
+			redirect_to root_path
+=end
+		else
+			render :new
+		end
+	end
+
+	private def secure_params
+		params.require(:contact).permit(:name, :email, :content)
+	end
+end
